5c3c035d5583bfaee761df2bf4e8bc2afbb42409
Sean <seanthewonderful@gmail.com>
2022-03-16 12:29:14 -0600

    switching to middleware

--- server/server.js
+++ server/server.js
@@ -6,13 +6,24 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/index.html"))
-})
+// Middleware
+app.use(express.static(path.join(__dirname, "../public")))
 
-app.get("/styles", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/index.css"))
-})
+
+// // Endpoints:
+// // app.get("/", function(req, res) {
+// //     res.sendFile(path.join(__dirname, "../public/index.html"))
+// // })
+
+// // app.get("/styles", (req, res) => {
+// //     res.sendFile(path.join(__dirname, "../public/index.css"))
+// // })
+
+//If pictures were in the local file and not online, link them from index.html locally
+//and this code is needed for heroku to be able to access it from /public folder.
+// app.get("/", (req, res) => {
+//     res.sendFile(path.join(__dirname, "../public"))
+// })
 
 const port = process.env.PORT || 4005
 
